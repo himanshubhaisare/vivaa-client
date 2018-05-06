c71aabecb89ea212c4285b7c062f5db6005eeae8
Gurugulaab <himanshu.bhaisare@gmail.com>
2018-05-05 19:33:56 -0700

    Fetching physicians

--- src/App.js
+++ src/App.js
@@ -9,11 +9,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
           <Physicians />
           <Appointments />
-        </p>
       </div>
     );
   }
--- src/components/physicians.js
+++ src/components/physicians.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import '../styles/physicians.scss';
+import fetch from 'node-fetch';
 
 class Physicians extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            physicians: null
+        }
+    };
+
+    componentDidMount = async () => {
+        const physicians = await fetch('http://localhost:4000/physicians')
+            .then(res => res.json());
+        this.setState({physicians})
+        console.log(physicians);
+    };
+
     render() {
-      return (
-        <div className="physicians">
-          These are physicians
-        </div>
-      );
+        const {physicians} = this.state;
+        return (
+            <div className="physicians">
+            {physicians && physicians.map( physician => {
+                return (<h5>Name : {physician.name}</h5>);
+            })}
+            </div>
+        );
     }
   }
   
