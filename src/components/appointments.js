6634e016645e8f3139189d5a39cf4aec814632b7
Gurugulaab <himanshu.bhaisare@gmail.com>
2018-05-17 21:13:23 -0700

    Notable onsite exercise

--- src/components/appointments.js
+++ src/components/appointments.js
@@ -12,7 +12,10 @@ class Appointments extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      appointments: null
+      appointments: null,
+      name: null,
+      time: null,
+      type: null
     }
   };
 
@@ -34,6 +37,44 @@ class Appointments extends Component {
     }
   };
 
+  deleteAppointment = async (e) => {
+    const id = e.currentTarget.value;
+    const {physicianId} = this.props;
+    if (id != null) {
+      await fetch(`http://localhost:4000/physicians/${physicianId}/appointments/${id}`, {method: 'delete'})
+      const appointments = await fetch(`http://localhost:4000/physicians/${physicianId}/appointments`)
+        .then(res => res.json());
+      this.setState({ appointments })
+    }
+  }
+
+  setName = (e) => {
+    const name = e.currentTarget.value;
+    this.setState({name});
+  }
+
+  setTime = (e) => {
+    const time = e.currentTarget.value;
+    this.setState({time});
+  }
+
+  setType = (e) => {
+    const type = e.currentTarget.value;
+    this.setState({type});
+  }
+
+  addAppointment = async () => {
+    const {name, time, type} = this.state;
+    const {physicianId} = this.props;
+    await fetch(`http://localhost:4000/physicians/${physicianId}/appointments`, {
+      method: 'post',
+      body: {name, time, type}});
+
+    const appointments = await fetch(`http://localhost:4000/physicians/${physicianId}/appointments`)
+      .then(res => res.json());
+    this.setState({ appointments })
+  }
+
   render() {
     const { appointments } = this.state;
     const {name} = this.props;
@@ -46,6 +87,7 @@ class Appointments extends Component {
           <div className="col"> Name </div>
           <div className="col"> Time </div>
           <div className="col"> Type </div>
+          <div className="col"/>
         </div>
         {appointments && appointments.map((appointment, i) => {
           return (
@@ -54,9 +96,29 @@ class Appointments extends Component {
               <div className="col"> {appointment.name} </div>
               <div className="col"> {appointment.time} </div>
               <div className="col"> {appointment.type} </div>
+              <button 
+                className="col" 
+                name="delete" 
+                type="button" 
+                value={appointment.id} 
+                onClick={this.deleteAppointment}> Delete 
+              </button>
             </div>
           );
         })}
+        <div className="appointment row" key={999}>
+              <div className="col"> {999} </div>
+              <div className="col"> <input type="text" placeholder="name" onChange={this.setName} /> </div>
+              <div className="col"> <input type="text" placeholder="time" onChange={this.setTime} /> </div>
+              <div className="col"> <input type="text" placeholder="type" onChange={this.setType} /> </div>
+              <button 
+                className="col" 
+                name="add" 
+                type="button" 
+                value={999} 
+                onClick={this.addAppointment}> Add 
+              </button>
+            </div>
       </div>
     );
   }
